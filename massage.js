ce5bc3823223299fb260d850a3e0ff55d3e8f313
Eishal <eishal.cecos@gmail.com>
2025-03-20 16:52:35 +0500

    m
    m

--- massage.js
+++ massage.js
@@ -1,2 +1,6 @@
 /*link:https://www.figma.com/design/zT5uhlDcwEaSqB600YnK3L/Untitled?node-id=0-1&p=f&t=ppiWEP0pyK7Rtapm-0*/
     // copy,link,ope,UI,UX   //
+
+    document.getElementById('alertButton').addEventListener('click', function() {
+        alert('Button was clicked!');
+    });
